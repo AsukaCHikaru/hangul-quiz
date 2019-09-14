15ee91722caf02cf99fd5177f33c56abc58ec047
asukachikaru <asukachikaru@gmail.com>
2019-09-14 14:58:31 +0900

    Record answer time

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div class="main-container">
-      <Quiz :quiz="currentQuiz[1]"/>
-      <input class="answer-input" type="text" v-model="answer" @keyup="checkAnswer">
+      <Quiz :quiz="currentQuiz.hangul"/>
+      <input class="answer-input" type="text" v-model="answer" @keyup.enter="checkAnswer">
     </div>
   </div>
 </template>
@@ -19,23 +19,47 @@ export default {
   },
   data: function () {
     return {
-      currentQuiz: this.pickNewHangul(),
-      answer: ""
+      currentQuiz: {
+        hangul: "",
+        spell: "",
+        startTime: "",
+        attampt: 0
+      },
+      answer: "",
+      history: []
     }
   },
   methods: {
     pickNewHangul: function () {
       return pick(hangul.double);
     },
-    checkAnswer: function () {
-      const correct = this.answer===this.currentQuiz[0];
-      if(correct) {
-        this.currentQuiz = this.pickNewHangul();
-        this.answer = "";
+    startNewQuiz: function () {
+      const newHangul = pick(hangul.double);
+      this.currentQuiz = {
+        hangul: newHangul[1],
+        spell: newHangul[0],
+        startTime: (new Date()).getTime()
       }
-      
+      this.answer = "";
+    },
+    checkAnswer: function () {
+      const correct = this.answer === this.currentQuiz.spell;
+      if(correct) {
+        const answerTime = (((new Date()).getTime() - this.currentQuiz.startTime) / 1000).toFixed(2);
+        this.history.push({
+          ...this.currentQuiz, answerTime: answerTime
+        });
+
+        this.startNewQuiz();
+      }
+      else {
+        this.currentQuiz.attampt++;
+      }
     }
-  }  
+  },
+  mounted: function () {
+    this.startNewQuiz();
+  }
 }
 </script>
 
@@ -56,7 +80,7 @@ body, html{
   background-color: transparent;
   border: solid 0px #fff6da;
   border-width: 0 0 3px 0;
-  color: #fff6da; 
+  color: #fff6da;
   font-size: 1.5em;
 }
 .answer-input:focus{
